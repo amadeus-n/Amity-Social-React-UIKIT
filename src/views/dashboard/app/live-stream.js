77a351e1ecfaf01cd39b9ac39d473599584bde9e
Au Thanakorn <thanakorn.t@amity.co>
2022-01-24 17:54:44 +0700

    yesss

--- src/views/dashboard/app/live-stream.js
+++ src/views/dashboard/app/live-stream.js
@@ -8,7 +8,7 @@ import {ReactFlvPlayer} from 'react-flv-player'
 const LiveStream =() =>{
     return(<div>
         <ReactFlvPlayer
-          url = "rtmp://live-ingest.upstra-china.cc/5fca0b4aa6a6c18d7615886c/1006af8fdeba02fd58bb62edd4d4de90?auth_key=1643031481-0-0-ef30072e1e8b5c51e26813baa294b9e5"
+          url = "http://live-stream.upstra-china.cc/5fca0b4aa6a6c18d7615886c/1006af8fdeba02fd58bb62edd4d4de90.flv?auth_key=1643031481-0-0-af31a0e674339d19c046bfc7e83f0ff8"
           heigh = "800px"
           width = "800px"
           isMuted={true}
